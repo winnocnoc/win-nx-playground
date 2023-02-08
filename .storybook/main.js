3730642232d6a0387bf96ddb20a17fe16c80a9c8
Win Eiwwongcharoen <113672716+winnocnoc@users.noreply.github.com>
2023-02-08 22:17:28 +0700

    revert

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,4 @@
 module.exports = {
   stories: [],
   addons: ['@storybook/addon-essentials'],
-  webpackFinal: async (config) => {
-    config.output.publicPath = '/win-nx-playground/';
-    return config;
-  },
 };
