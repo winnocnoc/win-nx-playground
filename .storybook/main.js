3f17b332aa50b3eb1f33aad0a8c0c4eb134a0f14
Win Eiwwongcharoen <113672716+winnocnoc@users.noreply.github.com>
2023-02-08 22:03:15 +0700

    change public path

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
   stories: [],
   addons: ['@storybook/addon-essentials'],
+  webpackFinal: async (config) => {
+    config.output.publicPath = '/win-nx-playground/';
+    return config;
+  },
 };
