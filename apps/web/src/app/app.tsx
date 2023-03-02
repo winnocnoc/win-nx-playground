6982d73b734f3e617727ebdaac77766fa0d7068d
Win Eiwwongcharoen <113672716+winnocnoc@users.noreply.github.com>
2023-03-02 12:30:28 +0700

    remove welcome

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,15 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Core } from '@rennerwin/core';
-import NxWelcome from './nx-welcome';
 
 export function App() {
-  return (
-    <>
-      <NxWelcome title="web" />
-      <div />
-      <Core />
-    </>
-  );
+  return <Core />;
 }
 
 export default App;
